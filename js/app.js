a9dd99f36fbfeb2f69e4623718cd7283540a3245
Sidney Williams <sydjana0911@gmail.com>
2023-04-27 20:22:54 -0400

    added problem

--- js/app.js
+++ js/app.js
@@ -13,9 +13,9 @@ Write a function called sum() that takes in two numbers as
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a,b) { //eslint-disable-line
   let sum =a+b;
-  let string = "The sum of 4 and 7 is 11.";
+  let string = `The sum of ${a} and ${b} is ${a+b}.`;
   return [sum, string];
 
 }
